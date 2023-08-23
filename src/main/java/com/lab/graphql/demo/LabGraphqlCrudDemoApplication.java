13da356c2084093c7edc43069fbad153d03dba84
wooshnp <nunojorgep@gmail.com>
2023-08-23 22:59:46 +0100

    first commit

---
+++ src/main/java/com/lab/graphql/demo/LabGraphqlCrudDemoApplication.java
@@ -0,0 +1,13 @@
+package com.lab.graphql.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabGraphqlCrudDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabGraphqlCrudDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lab/graphql/demo/LabGraphqlCrudDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab.graphql.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabGraphqlCrudDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
